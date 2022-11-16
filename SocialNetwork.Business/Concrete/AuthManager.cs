63ebf3fb8f4d204d787394631bb66d84224d9934
sananabbasov <sanan_abbasov@list.ru>
2022-11-16 12:57:23 +0400

    Data result features

---
+++ SocialNetwork.Business/Concrete/AuthManager.cs
@@ -0,0 +1,14 @@
+using SocialNetwork.Business.Abstract;
+using SocialNetwork.Entities.DTOs;
+using static SocialNetwork.Entities.DTOs.UserDTO;
+
+namespace SocialNetwork.Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        public LoginDTO Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
