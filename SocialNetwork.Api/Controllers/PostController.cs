60559c0cd65298368ff9458c15728258a87e3f8b
sananabbasov <sanan_abbasov@list.ru>
2022-11-29 22:13:48 +0400

    11.29.2022

---
+++ SocialNetwork.Api/Controllers/PostController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using SocialNetwork.Business.Abstract;
+using System.IdentityModel.Tokens.Jwt;
+using static SocialNetwork.Entities.DTOs.PostDTO;
+
+namespace SocialNetwork.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [Authorize]
+        [HttpPost("publishpost")]
+        public IActionResult AddPost(PostCreateDTO post)
+        {
+            var _bearer_token = Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
+            var handler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = handler.ReadJwtToken(_bearer_token);
+            var id = jwtSecurityToken.Claims.FirstOrDefault(x => x.Type == "nameid").Value;
+            _postService.CreatePost(post, Guid.Parse(id));
+            return Ok();
+        }
+
+    }
+}
---
+++ SocialNetwork.Api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Business.Abstract;
+
+namespace SocialNetwork.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Authorize]
+        [HttpGet("getuser")]
+        public IActionResult GetUserByEmail(string email)
+        {
+            return Ok(_userService.GetUserByEmail(email));
+        }
+    }
+}
